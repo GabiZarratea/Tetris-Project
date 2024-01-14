6c24b5b6a9b0cc1a8c7a93e044d641a9b865a2b9
Donato Zollo <davinazarra@gmail.com>
2024-01-13 22:06:38 -0300

    second commit

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const BLOCK_SIZE = 20
 const BOARD_WIDTH = 14
 const BOARD_HEIGHT = 30
 
+let score = 0
+
 canvas.width = BLOCK_SIZE * BOARD_WIDTH
 canvas.height = BLOCK_SIZE * BOARD_HEIGHT
 
@@ -53,7 +55,62 @@ const piece = {
     ]
 }
 
-function update(){
+const pieces = [
+    [
+        [1, 1],
+        [1, 1]
+    ],
+    [
+        [1],
+        [1],
+        [1],
+        [1]
+    ],
+    [
+        [1, 1, 1]
+    ],
+    [
+        [1, 1],
+        [1, 0],
+        [1, 0]
+    ],
+    [
+        [1, 1, 1],
+        [0, 1, 0]
+    ],
+    [
+        [1, 0],
+        [1, 0],
+        [1, 1]
+    ],
+    [
+        [1, 1, 0],
+        [0, 1, 1]
+    ],
+    [
+        [0, 1, 1],
+        [1, 1, 0]
+    ]
+]
+
+let dropCounter = 0
+let lastTime = 0
+
+function update(time = 0){
+   const deltaTime = time - lastTime
+   lastTime = time
+
+   dropCounter += deltaTime
+
+   if(dropCounter > 1000){
+    piece.position.y++
+    dropCounter = 0
+
+    if(checkCollision()){
+        piece.position.y--
+    }
+   }
+
     draw()
     window.requestAnimationFrame(update)
 }
@@ -82,9 +139,93 @@ function draw(){
 }
 
 document.addEventListener('keydown', event => {
-    if(event.key == 'ArrowLeft') piece. position.x--
-    if(event.key == 'ArrowRight') piece. position.x++
-    if(event.key == 'ArrowDown') piece. position.y++
+    if(event.key == 'ArrowLeft') {
+        piece.position.x--
+        if (checkCollision()){
+            piece.position.x++
+        }
+    }
+    if(event.key == 'ArrowRight'){
+        piece.position.x++
+        if (checkCollision()){
+            piece.position.x--
+        }
+    } 
+    if(event.key == 'ArrowDown'){
+        piece.position.y++
+        if (checkCollision()){
+            piece.position.y--
+            solidifyPiece()
+            removeRows()
+        }
+    }  
+    if(event.key == 'ArrowUp'){
+        const rotated = []
+    
+        for (let i = 0; i < piece.shape[0].length; i++){
+            const row = []
+
+            for(let a = piece.shape.length - 1; a >= 0; a--){
+                row.push(piece.shape[a][i])
+            }
+
+            rotated.push(row)
+        }
+
+        const previousShape = piece.shape
+        piece.shape = rotated
+        if(checkCollision()){
+            piece.shape = previousShape
+        }
+    }
 })
+
+function checkCollision () {
+    return piece.shape.find((row, y) => {
+        return row.find((value, x) => {
+            return (
+                value !== 0 &&
+                board[y + piece.position.y]?.[x + piece.position.x] !== 0
+            )
+        })
+    })
+}
+
+function solidifyPiece(){
+    piece.shape.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if(value == 1){
+                board[y + piece.position.y][x + piece.position.x] = 1
+            }
+        })
+    })
+
+    piece.position.x = Math.floor(BOARD_WIDTH / 2 - 2)
+    piece.position.y = 0
+
+    piece.shape = pieces[Math.floor(Math.random() * pieces.length)]
+
+    if(checkCollision()){
+        window.alert("Game over!! Sorry!")
+        board.forEach((row) => row.fill(0))
+    }
+}
+
+function removeRows(){
+    const rowsToRemove = []
+
+    board.forEach((row, y) => {
+        if(row.every(value => value == 1)){
+            rowsToRemove.push(y)
+        }
+    })
+
+    rowsToRemove.forEach(y => {
+        board.splice(y, 1)
+        const newRow = Array(BOARD_WIDTH).fill(0)
+        board.unshift(newRow)
+        score += 10
+    })
+}
 
 update()
