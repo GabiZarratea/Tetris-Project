2ce1908f9679b5fc69a977f4f63f712275b3e354
Gabriela Zarratea <71782924+GabiZarratea@users.noreply.github.com>
2024-01-16 20:41:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -107,10 +107,10 @@ let lastTime = 0
 
 let gameOver = false;
 
-document.addEventListener('DOMContentLoaded', function() {
-const storedRecord = JSON.parse(localStorage.getItem('tetrisRecord'));
-$record.innerText = `${storedRecord && storedRecord.score || 0}`;
-
+document.addEventListener('DOMContentLoaded', function() {    
+    const storedRecord = JSON.parse(localStorage.getItem('tetrisRecord'));
+    const recordScore = storedRecord ? storedRecord.score : 0;
+    $record.innerText = `${recordScore}`;
 });
 
 $restartButton.addEventListener('click', () => {
